a845771c64a1b29ec8a961041acf6a1ea1dc00c0
deep-quality-dev <hungrywarrior081@gmail.com>
2021-05-21 14:33:45 -0400

    Update API

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 const express = require('express')
-const { path } = require('express/lib/application')
+const bodyParser = require('body-parser')
 const {create} = require('ipfs-http-client')
+const fileUpload = require('express-fileupload')
+const fs = require('fs')
 
-const ipfs = create("http://localhost:5001")
+const ipfs = create("http://localhost:5002")
 const app = express()
 
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(fileUpload())
 
 app.get('/', (req, res) => {
-    return res.send('Welcome to my IPFS app')
+    res.render('home')
+    // return res.send('Welcome to my IPFS app')
 })
+
+const uploadFile = ({file, fileName}) => {
+    const filePath = 'files/' + fileName
+    
+    return new Promise(res => {
+        file.mv(filePath, async (err) => {
+            if (err) {
+                console.log('Error: failed to download the file');
+                return res.status(500).send(err)
+            }
+
+            const fileHash = await addFile({fileName, filePath})
+            fs.unlink(filePath, (err) => {
+                if (err) console.log(err)
+            })
+
+            
+            res(`https://gateway.ipfs.io/ipfs/${fileHash}`)
+        })
+    })
+    // return res.send("Thanks for submitting");
+}
+
+const addFile = async ({fileName, filePath}) => {
+    const fileContent = fs.readFileSync(filePath)
+    const file = {path: fileName, content: Buffer.from(fileContent)}
+    const filesAdded = await ipfs.add(file)
+    // console.log(filesAdded)
+    // QmYarjYDXfwcCBFmrk9RKAEfZ9NGYHokrtXRJkw1bkbsJv
+    return filesAdded.cid
+}
 
 app.post('/upload', async (req, res) => {
-    const data = req.body
-    console.log(data)
-    const fileHash = await addFile(data)
-    return res.send(`https://gateway.ipfs.io/ipfs/${fileHash}`)
+    const data = req.query
+    const file = req.files.file
+    const fileName = req.files.file.name
+
+    const imageHash = await uploadFile({file, fileName})
+
+    data.imageHash = imageHash
+    // console.log(data)
+
+    const nftHash = await addProfile(data)
+    return res.send(`https://gateway.ipfs.io/ipfs/${nftHash}`)
 })
 
-const addFile = async ({path, content}) => {
-    const file = {path: path, content: Buffer.from(content)}
-    const filesAdded = await ipfs.add(file)
+const addProfile = async (data) => {
+    const nft = {path: data.path, content: JSON.stringify(data)}
+    const filesAdded = await ipfs.add(nft)
     console.log(filesAdded)
     // QmYarjYDXfwcCBFmrk9RKAEfZ9NGYHokrtXRJkw1bkbsJv
     return filesAdded.cid
